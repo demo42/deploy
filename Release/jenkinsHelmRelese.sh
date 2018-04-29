4aa1e4ce389f6d1ae32be5f9a959ab36ac91b993
Steve Lasker <StevenLasker@Hotmail.com>
2018-04-29 16:02:40 -0700

    Refactor KUBECONFIG settings for each release

--- Release/jenkinsHelmRelese.sh
+++ Release/jenkinsHelmRelese.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Script used in Jenkins to do a Helm Upgrade
-export KUBECONFIG=/home/stevelas/k8s-configs/acrdemoeus
+# Presumes the path to KUBECONFIG has been set to the active cluster
+# In a multi-geo deployment, each jenkins job would set this to it's region, in the release defintion
+# export KUBECONFIG=/home/stevelas/k8s-configs/acrdemoeus
 
 echo REGISTRY:$REGISTRY
 echo REPOSITORY:$REPOSITORY
