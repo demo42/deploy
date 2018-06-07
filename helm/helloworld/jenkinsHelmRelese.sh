f833261af8f1ef525fd6296336a6e230cfeb9622
Steve Lasker <StevenLasker@Hotmail.com>
2018-06-07 09:02:45 -0700

    add hellowrold, refactor demo42 important chart

---
+++ helm/helloworld/jenkinsHelmRelese.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script used in Jenkins to do a Helm Upgrade
+# Presumes the path to KUBECONFIG has been set to the active cluster
+# In a multi-geo deployment, each jenkins job would set this to it's region, in the release defintion
+# export KUBECONFIG=/home/stevelas/k8s-configs/acrdemoeus
+
+echo REGISTRY:$REGISTRY
+echo REPOSITORY:$REPOSITORY
+echo TAG:$TAG
+pwd
+case $REPOSITORY in
+  demo42/helloworld*)
+    echo helm upgrade helloworld . --reuse-values --set helloworld.image=$REGISTRY/$REPOSITORY:$TAG 
+    helm upgrade helloworld . --reuse-values --set helloworld.image=$REGISTRY/$REPOSITORY:$TAG 
+
+  ;;
+  *)
+    echo helm upgrade helloworld ./release/helm/ --reuse-values 
+    helm upgrade helloworld ./release/helm/ --reuse-values
+  ;;
+esac
